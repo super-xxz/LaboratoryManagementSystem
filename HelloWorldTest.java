4465969639529e7618087a777596cdcbe6f73608
superx <xuxuezhi77@gmail.com>
2022-10-26 21:34:24 +0800

    test push /by superx 2022/10/26

---
+++ HelloWorldTest.java
@@ -0,0 +1,13 @@
+/**
+ * <p>
+ * </p>
+ *
+ * @author xxz
+ * @data 2022/10/26 21:31
+ */
+//这是一个测试idea ide git推送功能的测试java 它没有任何意义
+public class HelloWorldTest {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
